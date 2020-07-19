b9888e202ad09d42d41f87a1acd872e6206a4223
michaels107 <michaels.107@osu.edu>
2020-07-19 19:54:56 -0400

    added my 3 tables

--- Collaborate/db/schema.rb
+++ Collaborate/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_015711) do
+ActiveRecord::Schema.define(version: 2020_07_19_230744) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,35 @@ ActiveRecord::Schema.define(version: 2020_07_19_015711) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "apart_ofs", force: :cascade do |t|
+    t.integer "name_dot_number_id"
+    t.integer "group_name_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_name_id"], name: "index_apart_ofs_on_group_name_id"
+    t.index ["name_dot_number_id"], name: "index_apart_ofs_on_name_dot_number_id"
+  end
+
+  create_table "assisted_bies", force: :cascade do |t|
+    t.integer "email_professor_id"
+    t.integer "email_ta_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_professor_id"], name: "index_assisted_bies_on_email_professor_id"
+    t.index ["email_ta_id"], name: "index_assisted_bies_on_email_ta_id"
+  end
+
+  create_table "gives", force: :cascade do |t|
+    t.integer "id_id"
+    t.integer "name_dot_number_id"
+    t.integer "email_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_id"], name: "index_gives_on_email_id"
+    t.index ["id_id"], name: "index_gives_on_id_id"
+    t.index ["name_dot_number_id"], name: "index_gives_on_name_dot_number_id"
   end
 
   create_table "peer_evaluations", force: :cascade do |t|

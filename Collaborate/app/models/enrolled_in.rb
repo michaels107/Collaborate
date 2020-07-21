ebce900e00f11e8a0e32793aacae69d33250b15d
CarolineWheeler <wheeler.1017@osu.edu>
2020-07-21 13:29:53 -0400

    implementation of taught_by.rb and enrolled_in.rb

--- Collaborate/app/models/enrolled_in.rb
+++ Collaborate/app/models/enrolled_in.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 # Created 7/20/2020 by Caroline Wheeler
-# Validation rules for insertions into enrolled_in table
+# Validation rules for insertions into EnrolledIn table
 class EnrolledIn < ApplicationRecord
   belongs_to :student
   belongs_to :course
-  validates :student_id, presence: true, uniqueness: true
-  validates :course_id, presence: true, uniqueness: true
+  validates :id, presence: true, uniqueness: true
 end

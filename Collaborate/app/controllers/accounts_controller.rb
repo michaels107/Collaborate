a72c2c1684917489be92275c2267e2ce665466b9
Duytan Tran <tran.732@osu.edu>
2020-07-27 12:50:33 -0400

    Add redirect to course page if professor is logged in

--- Collaborate/app/controllers/accounts_controller.rb
+++ Collaborate/app/controllers/accounts_controller.rb
@@ -13,20 +13,20 @@ class AccountsController < ApplicationController
     params.permit!
     para = params[:account]
     @account = Account.new(email: para[:email], password: para[:password])
-    if @account.save
-      associated = if params[:role] == 'student'
-                     Student.new(account_id: Account.order('created_at').last.id, f_name: para[:f_name],
-                                 l_name: para[:l_name], dot_name: para[:dot_name].downcase, rating: 'A')
-                   else
-                     Professor.new(account_id: Account.order('created_at').last.id, f_name: para[:f_name],
-                                   l_name: para[:l_name])
-                   end
-      if associated.valid?
-        associated.save
-        redirect_to :authenticated_root
-      else
-        @account.delete
-      end
+    return unless @account.save
+
+    associated = if params[:role] == 'student'
+                   Student.new(account_id: Account.order('created_at').last.id, f_name: para[:f_name],
+                               l_name: para[:l_name], dot_name: para[:dot_name].downcase, rating: 'A')
+                 else
+                   Professor.new(account_id: Account.order('created_at').last.id, f_name: para[:f_name],
+                                 l_name: para[:l_name])
+                 end
+    if associated.valid?
+      associated.save
+      redirect_to :authenticated_root
+    else
+      @account.delete
     end
   end
 end
--- Collaborate/app/controllers/pages_controller.rb
+++ Collaborate/app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
+# Created 7/26/2020 by Sean Michaels
+# Edited 7/27/2020 by Duytan Tran: added redirect if professor is signed in
+# Controller for managing student view of courses
 class PagesController < ApplicationController
   def student_home
+    if Professor.exists? account_id: current_account.id
+      redirect_to courses_path
+      return
+    end
     @student = Student.find_by(account_id: current_account.id)
     course_ids = EnrolledIn.where(student_id: @student.id).pluck :course_id
-    @enrolled_in= Course.where id: course_ids
+    @enrolled_in = Course.where id: course_ids
   end
 end

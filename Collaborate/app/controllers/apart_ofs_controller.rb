8f709c6a621d6063441d44cd0c8ede5f712ded9e
reema201 <gupta.949@osu.edu>
2020-07-28 14:53:16 +0530

    added file level comments

--- Collaborate/app/controllers/apart_ofs_controller.rb
+++ Collaborate/app/controllers/apart_ofs_controller.rb
@@ -1,3 +1,5 @@
+# Edited 7/27/2020 by Reema Gupta
+# Controller for student enrollment in a group
 class ApartOfsController < ApplicationController
   def add_student_to_group
     student = EnrolledIn.find_by(student_id: params[:student_id], course_id: params[:course_id])
--- Collaborate/app/controllers/groups_controller.rb
+++ Collaborate/app/controllers/groups_controller.rb
@@ -1,3 +1,5 @@
+# Created 7/27/2020 by Reema Gupta
+# Controller for the group page
 class GroupsController < ApplicationController
   def new
     @group=Group.new
